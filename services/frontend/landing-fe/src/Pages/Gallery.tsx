7d227d1ce86d41e522304d09668022ae50b77fc3
KaranSJ22 <karanjamkhandi@gmail.com>
2025-06-26 02:24:35 +0530

    Navbar-Footer

--- services/frontend/landing-fe/src/Pages/Gallery.tsx
+++ services/frontend/landing-fe/src/Pages/Gallery.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { motion, useMotionValue, useMotionTemplate } from "framer-motion";
 import "./Gallery.css";
 import bckimg from "../assets/bckimg.jpg";
-import { Card } from "@/component/luxe/ui/card";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Particles from "../Components/Particles";
